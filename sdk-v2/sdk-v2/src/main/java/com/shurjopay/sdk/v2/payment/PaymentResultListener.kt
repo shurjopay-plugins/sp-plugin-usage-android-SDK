e8808bb7b3a03862e88f0dc7db39ccebf3d1b23b
Rz Rasel <emailrzrasel@gmail.com>
2022-09-27 18:17:09 +0600

    Update git content

--- sdk-v2/sdk-v2/src/main/java/com/shurjopay/sdk/v2/payment/PaymentResultListener.kt
+++
@@ -1,9 +0,0 @@
-package com.shurjopay.sdk.v2.payment
-
-import com.shurjopay.sdk.v2.model.ErrorSuccess
-
-interface PaymentResultListener {
-    fun onSuccess(errorSuccess: ErrorSuccess)
-    fun onFailed(errorSuccess: ErrorSuccess)
-    fun onBackButtonListener(errorSuccess: ErrorSuccess): Boolean
-}
